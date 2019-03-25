674d36eff476764a5e437e00ba9f0c9ad630e103
Trace Rainbolt <tracerainbolt@gmail.com>
2019-03-24 19:50:52 -0700

    Moving to timestep based training.

--- generate_dataset.py
+++ generate_dataset.py
@@ -18,7 +18,7 @@ rbs_and_wrs = player_df.loc[(player_df['pos1'].isin(['RB', 'WR']))]
 REG_SEASON_WEEKS = 17
 
 # Fields we care about for training
-TRAINING_FIELDS = ['player', 'ra', 'sra', 'fp', 'recy', 'conv', 'tdrec', 'sra', 'ry', 'tdr', 'fuml', 'rec', 'trg', 'ret', 'rety', 'tdret']
+TRAINING_FIELDS = ['player', 'ra', 'sra', 'fp3', 'recy', 'conv', 'tdrec', 'sra', 'ry', 'tdr', 'fuml', 'rec', 'trg', 'ret', 'rety', 'tdret']
 
 # Where to save the dataset
 out_file = 'dataset.npy'
@@ -31,50 +31,37 @@ player_dict = player_df['player'].to_dict()
 team_dict = {v: k for k, v in team_dict.items()}
 player_dict = {v: k for k, v in player_dict.items()}
 
+
 def generate_dataset():
-    dataset = None
-    stats_df = None
-    total = 0
+    all_weeks = None
+    dataset = []
     for _, week in schedule_df.groupby(['seas', 'wk']):
         week_num = week['wk'].iloc[0]
+        seas_num = week['seas'].iloc[0]
 
-        if week_num > REG_SEASON_WEEKS:
-            stats_df = None
-            continue
+        if seas_num == 2018:
+            break
 
-        extra_stats, weekly_stats = get_weekly_stats(week)
+        weekly_stats = get_weekly_stats(week)
 
-        if stats_df is not None:
-            stats_df['opponent'] = extra_stats['opponent']
-            stats_df['team'] = extra_stats['team']
-            stats_df['home'] = extra_stats['home']
-            stats_df['player_id'] = extra_stats['player_id']
-            stats_df = stats_df.dropna(subset=['opponent'])
-            stats_df['week_num'] = week_num
-            stats_df['actual_fp'] = weekly_stats['fp']
-
-            if dataset is None:
-                dataset = stats_df.values
-            else:
-                dataset = np.append(dataset, stats_df.values, axis=0)
-
-            stats_df = stats_df.drop(['opponent', 'team','actual_fp', 'week_num', 'home', 'player_id'], axis=1)
-
-        weighted_data = weekly_stats * (week_num / REG_SEASON_WEEKS)
-        if stats_df is None:
-            stats_df = weighted_data
+        if all_weeks is None:
+            all_weeks = weekly_stats
         else:
-            stats_df = stats_df.add(weighted_data, fill_value=0)
+            all_weeks = all_weeks.append(weekly_stats)
 
-        stats_df = stats_df / week_num
+    for _, player in all_weeks.groupby('player'):
+        data = player.drop('player', axis=1).values
+        dataset.append(data)
+
     return dataset
 
 
 
 def get_weekly_stats(week):
     week_num = week['wk'].iloc[0]
+    seas_num = week['seas'].iloc[0]
     data_df = None
-    extra_df = None
+    stats_df = None
     for index, game in week.iterrows():
         gid = game['gid']
         new_players = offense_df.loc[offense_df['gid'] == gid]
@@ -88,7 +75,7 @@ def get_weekly_stats(week):
         if data_df is None:
             data_df = new_data
         else:
-            data_df = data_df.add(new_data * (week_num / REG_SEASON_WEEKS), fill_value=0)
+            data_df = data_df.add(new_data, fill_value=0)
 
         teams = new_players[['player', 'team']]
         teams = teams.set_index('player', drop=False)
@@ -105,17 +92,17 @@ def get_weekly_stats(week):
         teams['home'] = home
         teams['player_id'] = player_ids
 
-        if extra_df is None:
-            extra_df = teams
+        if stats_df is None:
+            stats_df = teams
         else:
-            extra_df = extra_df.append(teams)
+            stats_df = stats_df.append(teams)
 
-    return extra_df, data_df
-
-
+    data_df = data_df.merge(stats_df, how='left', on='player')
+    data_df['week'] = week_num
+    return data_df
 
 
 if __name__ == '__main__':
     dataset = generate_dataset()
     np.save(out_file, dataset)
-    print(f'Successfully generated dataset with {dataset.shape[0]} entries.')
+    print(f'Successfully generated dataset with {len(dataset)} entries.')
--- train.py
+++ train.py
@@ -2,38 +2,69 @@ import numpy as np
 from keras import models, layers
 from keras.utils import normalize
 
-EPOCHS = 80
+EPOCHS = 200
 
-dataset = np.load('dataset.npy')
+POINT_INDEX = 2
 
-print(dataset)
+def main():
+    dataset = np.load('dataset.npy')
+    x, y = generate(dataset)
+    train(x, y)
 
-label_column = dataset.shape[1] - 1
-input_data = dataset[:, 0: label_column]
-targets = dataset[:, label_column]
 
-input_data = normalize(
-    input_data.astype('float32'),
-    axis=-1,
-)
+def train(x, y):
+    # input_data = normalize(
+    #     x.astype('float32'),
+    #     axis=-1,
+    # )
+    model = models.Sequential()
+    model.add(layers.LSTM(64, input_shape=(None, 20)))
+    model.add(layers.Dense(1))
+    print(model.summary())
 
-model = models.Sequential()
-model.add(layers.Dropout(0.2, input_shape=(input_data.shape[1],)))
-model.add(layers.Dense(256, activation='relu'))
-model.add(layers.Dense(128, activation='relu'))
-model.add(layers.Dense(64, activation='relu'))
-model.add(layers.Dense(32, activation='relu'))
-model.add(layers.Dense(1))
-print(model.summary())
+    model.compile(optimizer='adam', loss='mae',
+                  metrics=['mae'])
 
-model.compile(optimizer='adam', loss='mae',
-              metrics=['mae'])
+    history = model.fit(
+        x=x,
+        y=y,
+        validation_split=.25,
+        epochs=EPOCHS,
+        batch_size=16)
 
-history = model.fit(
-    x=input_data,
-    y=targets,
-    validation_split=.25,
-    epochs=EPOCHS,
-    batch_size=32)
+    model.save('offensive_player_fp_predict.h5')
 
-model.save('offensive_player_fp_predict.h5')
+
+def generate(dataset):
+    samples = []
+    targets = []
+    for player in dataset:
+        prev_week = 0
+        data = []
+        for week in player:
+            if week[-1] < prev_week:
+                perm_data, target_vals = perm(data)
+                samples.extend(perm_data)
+                targets.extend(target_vals)
+                data = []
+            data.append(week)
+            prev_week = week[-1]
+
+    return samples, targets
+
+
+def perm(data):
+    perms = []
+    targets = []
+    for i, _ in enumerate(data):
+        try:
+            targets.append(data[i + 1][POINT_INDEX])
+        except IndexError:
+            continue
+        perms.append(data[:i + 1])
+
+    return perms, targets
+
+
+if __name__ == '__main__':
+    main()
